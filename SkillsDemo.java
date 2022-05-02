f0ce1a111195521611c2e7995b9aecd2fce58563
cathyiic <103155841+cathyiic@users.noreply.github.com>
2022-05-01 23:42:48 -0700

    failing test

--- SkillsDemo.java
+++ SkillsDemo.java
@@ -1,2 +1,5 @@
 class SkillsDemo{
+    public static int multiply(int n, int m){
+        return n*m;
+    }
 }
---
+++ SkillsDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillsDemoTest {
+    @Test
+    public void multiply(){
+        assertEquals(1, SkillsDemo.multiply(2, 3)); //failure
+    }
+
+    
+}
